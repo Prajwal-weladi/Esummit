c8684ab525ee7e8f8dab6fa3cd742400dd1d37da
adityakale3115 <aditya.kale0405@gmail.com>
2025-03-09 13:30:52 +0530

    issues solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-<<<<<<< HEAD
-import About from "./components/About";
-import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
-import Features from "./components/Features";
-import Story from "./components/Story";
-import Contact from "./components/Contact";
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar"; // Ensure filename consistency
+import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
 import Etalks from "./components/Etalks";
->>>>>>> aeddb8e (Pop - Up removed and done with Etalk Page)
 import Footer from "./components/Footer";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar />
-      <Hero />
-      <About />
-      <Features />
-      <Story />
-      <Contact />
-      <Footer />
-    </main>
-=======
     <Router>
       <main className="relative min-h-screen w-screen overflow-x-hidden" role="main">
         <Navbar />
@@ -51,7 +31,6 @@ function App() {
         </Routes>
       </main>
     </Router>
->>>>>>> aeddb8e (Pop - Up removed and done with Etalk Page)
   );
 }
 
